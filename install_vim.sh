b570382c5ce365f618b2dd6ff6ac130f11711640
Antonio Sánchez Reynaga <antonio@bloq.dev>
2021-05-22 16:39:22 -0500

    Fix installer

--- install_vim.sh
+++ install_vim.sh
@@ -7,9 +7,8 @@ cp ~/.config/nvim ~/.config/nvim_old
 
 echo "=> Moving files"
 cp -Rv nvim/*.vim ~/.config/nvim/
-cp -Rv nvim/plug_config ~/.config/nvim/plug_config
-cp -Rv nvim/lua ~/.config/nvim/lua
+cp -Rv nvim/plug_config/ ~/.config/nvim/plug_config/
+cp -Rv nvim/lua/ ~/.config/nvim/lua/
 cp -Rv .ideavimrc ~/.ideavimrc
 
 echo "DONE"
-
